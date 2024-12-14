5e5151fa65a748515c6f2010cd92660814cbf8c0
Mut-Surini <fitrahqareem9@gmail.com>
2024-12-14 10:15:40 +0700

    commit

--- app.js
+++ app.js
@@ -6,6 +6,26 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/petisi', (req, res) => {
+    res.send('About us');
+});
+
+app.get('/laporan', (req, res) => {
+    res.send('About us');
+});
+
+app.get('/chatbot', (req, res) => {
+    res.send('About us');
+});
+
+app.get('/tambahPetisi', (req, res) => {
+    res.send('About us');
+});
+
+app.get('/tambahLaporan', (req, res) => {
+    res.send('About us');
+});
+
 app.listen(port, () => {
     console.log('Example app listening on port 3000!');
 });
